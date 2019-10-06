13de044896179004c3cb30b8146da71d4b5cecd4
Cinthya Villegas <cinthyagpe.villegas@gmail.com>
2019-10-06 16:55:22 -0500

    Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name= "lilyac",
+    version="0.0.20",
+    description= (),
+    authors= ("Heladio Amaya", "Cinthya Villegas", "Jose Quiroga"),
+    authors_email= ("heladio.ac@gmail.com", "cinthyagpe.villegas@gmail.com", "no me sé el correo de Quiroga"),
+    packages= ["lilyac"],
+    install_requires= [],
+)
